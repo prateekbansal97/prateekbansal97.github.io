8408eba51938652d6507ca0d539eaa9f86796719
Prateek Bansal <prateekbansal97@gmail.com>
2025-11-05 15:43:48 -0600

    Added workflows

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import tailwind from '@astrojs/tailwind';
 import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
-  site: 'https://your-domain.com', // update for canonical
+  site: 'https://prateekbansal97.github.io/', // update for canonical
   integrations: [
     tailwind({
       applyBaseStyles: false // we handle our own base for academic typography
